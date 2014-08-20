0354e8e473e35fa0c869986ea516047648aa68a7
Mubarak Mohideen N <disfrutarz@gmail.com>
2014-08-20 21:47:59 +0530

    changes

--- UIMockupDesign/src/com/example/uimockupdesign/adapter/Item.java
+++
@@ -1,8 +0,0 @@
-package com.example.uimockupdesign.adapter;
-
-import android.view.View;
-
-public interface Item {
-    public int getViewType();
-    public View getView(View convertView,int position);
-}
